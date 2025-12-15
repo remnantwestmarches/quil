3655aa6bb6bc44bf1a1b129bec3dfd9964e80592
Lucien Breitkopf <lucien.breitkopf@hotmail.com>
2025-12-15 19:26:12 +0100

    more debug cleanup

--- src/commands/charinfo.ts
+++ src/commands/charinfo.ts
@@ -1,7 +1,6 @@
 // commands/charinfo.ts
 import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
 import { showCharacterEmbed } from "../utils/embeds.js";
-import { LootService } from "../domain/mip.js";
 
 export const data = new SlashCommandBuilder()
   .setName("charinfo")
@@ -10,17 +9,5 @@ export const data = new SlashCommandBuilder()
   .addStringOption((o) => o.setName("character").setDescription("Target character").setAutocomplete(true));
 
 export async function execute(interaction: ChatInputCommandInteraction) {
-  
-  //const table = "Table G1"; // Example input
-  //const type = "gambling";
-  //const rolls = 5;
-
-  //const lootBot = new LootService();
-  
-  //await lootBot.loadTables();
-
-  //const itemsRolled = lootBot.processCommand(table, type, rolls);
-
-  //showCharacterEmbed(interaction, {title: `Rolled ${rolls} time${rolls > 1 ? "s" : ""} on ${table} (${type}):`, desc:`${itemsRolled.join('\n')}`})
   showCharacterEmbed(interaction)
 }
